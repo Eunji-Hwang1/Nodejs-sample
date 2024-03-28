6da5265477b5be02fca4b5f4383224b09d9a749a
root <root@NAHA>
2024-03-28 14:00:54 +0900

    프런트 단 javascript 파일 만들기

---
+++ app/app.js
@@ -0,0 +1,31 @@
+// const http = require("http");
+// const app = http.createServer((req, res) => {
+//     res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+//     if (req.url === "/") {
+//         res.end("루트");
+//     } else if (req.url === "/login") {
+//         res.end("로그인");
+//     }
+// });
+
+// app.listen(3001, () => {
+//     console.log("http 서버");
+// });
+
+"use strict";
+
+//모듈
+const express = require("express");
+const app = express();
+
+//라우팅
+const home = require("./src/routes/home");
+
+// app setting
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
+app.use(express.static(`${__dirname}/src/public`));
+
+app.use("/", home); //use -> 미들 뭬어 등록 매서드
+
+module.exports = app;
